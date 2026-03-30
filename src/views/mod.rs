f39afc9f46d0fafe912bd3f50d4c9ec400cb3eb3
Simon Nilsson <52958264+fitinghof@users.noreply.github.com>
2026-03-30 21:09:40 +0200

    login (#1)
    Co-authored-by: Cedian03 <nyaalgotjohansson@gmail.com>

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,4 +1,12 @@
 use leptos::prelude::*;
+use leptos::task::spawn_local;
+
+use matrix_sdk::{
+    Client,
+    ruma::{UserId, user_id},
+};
+
+use crate::ClientContext;
 
 stylance::import_style!(style, "../styles/style.module.scss");
 
@@ -12,11 +20,41 @@ pub fn side_bar() -> impl IntoView {
 }
 #[component]
 pub fn login() -> impl IntoView {
-    let (stage, set_stage) = signal(0);
+    let user = RwSignal::new("".to_owned());
+    let password = RwSignal::new("".to_owned());
+
+    let client_context = expect_context::<RwSignal<Option<ClientContext>, LocalStorage>>();
+
+    let onclick = async move || {
+        let user_string = format!("@{}:{}", user.get(), "matrix.org");
+
+        let user = <&UserId>::try_from(user_string.as_str()).unwrap();
+
+        let client_res = Client::builder()
+            .server_name(user.server_name())
+            .build()
+            .await;
+
+        if let Ok(client) = client_res {
+            client
+                .matrix_auth()
+                .login_username(user, &password.get())
+                .send()
+                .await
+                .unwrap();
+
+            client_context.set(Some(ClientContext(client)));
+        }
+    };
 
     view! {
         <div>
-            <input></input>
+            <input bind:value=user></input>
+            <input type="password" bind:value=password></input>
+            <button on:click=move |_| {
+                    spawn_local(async move {onclick().await;
+                });
+            }>Button</button>
         </div>
     }
 }
