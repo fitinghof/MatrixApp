b468f522a3eb484c162e4eee7d2842d94a44e90c
Simon F Nilsson <simon.f.nilsson@me.com>
2026-03-30 21:04:59 +0200

    login
    Co-authored-by: Cedian03 <nyaalgotjohansson@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,31 @@
 mod views;
 use leptos::prelude::*;
-use leptos::task::spawn_local;
-use matrix_sdk::{
-    Client,
-    config::SyncSettings,
-    ruma::{events::room::message::SyncRoomMessageEvent, user_id},
-};
+use matrix_sdk::ruma::user_id;
 
 stylance::import_style!(style, "styles/style.module.scss");
 
+#[derive(Clone, Debug)]
+#[repr(transparent)]
+pub struct ClientContext(matrix_sdk::Client);
+
 #[component]
 fn App() -> impl IntoView {
-    let user = user_id!("@sibbeeegold:matrix.org");
+    let client: RwSignal<Option<ClientContext>, LocalStorage> = RwSignal::new_local(None);
 
-    let test = LocalResource::new(async move || {
-        Client::builder()
-            .server_name(user.server_name())
-            .build()
-            .await
-    });
-
-    let (some, set_some) = signal(0);
+    provide_context(client);
 
     view! {
-        <div>
-            {move || {
-                match async_data.read() {
-                    Some(client) => view! { <p>"Client ready!"</p> }.into_view(),
-                    None => view! { <p>"Loading client..."</p> }.into_view(),
-                }
-            }}
-        </div>
-        <views::SideBar/>
+        <Show
+         when=move || client.get().is_none()
+         >
+            <views::Login/>
+         </Show>
+        <Show
+         when=move || client.get().is_some()
+         >
+            <views::SideBar/>
+         </Show>
+
     }
 }
 
